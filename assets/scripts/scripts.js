ee38b95c1920b21a44bbd0d3e1daff61d186ed57
Bluiss <135335614+Bluiss@users.noreply.github.com>
2023-09-25 19:12:52 +0100

    add testing array for set next question and add set next question function

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,12 +1,33 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 
+let shuffleQuestions,currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
 
 function startGame(){
     console.log('started')
     startButton.classList.add('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0 
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 
+
+
+
+
+
+
+
+const questions = [
+    {
+        question: 'what is two + two',
+        answers: [
+            { text: 4, correct: true},
+            { text: 22, correct: false}
+        ]
+    },
+]
